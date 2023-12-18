71ed0152b5883cc12405505550306831aabaa810
Lenard <bebobhd@gmail.com>
2023-12-18 20:20:39 +0000

    added User and Message types

--- concurrency/src/Lib.hs
+++ concurrency/src/Lib.hs
@@ -1,6 +1,21 @@
 module Lib
-    ( someFunc
+    ( User
+    , Message
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type Name = String
+
+data User = User {
+    username :: Name,
+    messagesSent :: Int,
+    messagesRecieved :: Int
+}
+
+data Message = Message {
+    content :: String,
+    sender :: User,
+    recipient :: User
+}
