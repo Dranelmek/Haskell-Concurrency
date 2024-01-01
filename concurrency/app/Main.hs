80ed0f4e9b9fb573875418178fd66bf6d85288b5
Lenard K <lendevacc@gmail.com>
2024-01-01 20:30:00 +0000

    added hello world and TODO

--- concurrency/app/Main.hs
+++ concurrency/app/Main.hs
@@ -1,6 +1,12 @@
 module Main (main) where
 
 import Lib
+import Control.Concurrent
 
 main :: IO ()
-main = someFunc
+main = do 
+    putStrLn "hello world!"
+
+{-| make a message list the shared variable so that a user-thread takes it and adds a message to it that way each user can check if the list is 100 long
+    if it is the program can terminate.
+    -}
