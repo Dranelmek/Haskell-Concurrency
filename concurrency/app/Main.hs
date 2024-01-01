d295a155b1c32f5e138a2f3e45a2dd94be4b2bb6
Lenard K <lendevacc@gmail.com>
2024-01-01 20:38:13 +0000

    added userProcess template

--- concurrency/app/Main.hs
+++ concurrency/app/Main.hs
@@ -2,11 +2,16 @@ module Main (main) where
 
 import Lib
 import Control.Concurrent
+import GHCi.Message (Message)
+import Distribution.Compat.Prelude (undefined)
 
 main :: IO ()
 main = do 
     putStrLn "hello world!"
 
-{-| make a message list the shared variable so that a user-thread takes it and adds a message to it that way each user can check if the list is 100 long
-    if it is the program can terminate.
+{-| TODO: make a message list the shared variable so that a user-thread takes it and adds a message to it that way each user can check if the list is 100 long
+    if it is the program can terminate. 
     -}
+
+userProcess :: User -> MVar [Message] -> IO ()
+userProcess = undefined
