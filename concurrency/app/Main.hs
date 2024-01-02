940e7fafd42fa61fccb126bafeafe2d91c2a06ae
Lenard K <lendevacc@gmail.com>
2024-01-02 22:17:28 +0000

    added sendmessage func

--- concurrency/app/Main.hs
+++ concurrency/app/Main.hs
@@ -2,8 +2,6 @@ module Main (main) where
 
 import Lib
 import Control.Concurrent
-import GHCi.Message (Message)
-import Distribution.Compat.Prelude (undefined)
 
 main :: IO ()
 main = do 
@@ -11,7 +9,14 @@ main = do
 
 {-| TODO: make a message list the shared variable so that a user-thread takes it and adds a message to it that way each user can check if the list is 100 long
     if it is the program can terminate. 
+
+    add receiveMessage function
     -}
 
-userProcess :: User -> MVar [Message] -> IO ()
+-- | create user thread
+userProcess :: User -> MVar Message -> MVar [Message] -> IO ()
 userProcess = undefined
+
+-- | manipulate shared memory to send message
+sendMessage :: MVar Message -> MVar [Message] -> IO ()
+sendMessage = undefined
